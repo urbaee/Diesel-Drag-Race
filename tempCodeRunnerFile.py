9ce9eb43745c0477de7ec9e1cc04573efbfd7038
Rahmat Aldi Nasda <145269283+urbaee@users.noreply.github.com>
2024-05-28 00:48:44 +0700

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-for event in pygame.event.get():
-                            if event.type == pygame.QUIT:
-                                run = False
-                            elif event.type == pygame.KEYDOWN:
-                                if event.key == pygame.K_x:
-                                    pygame.mixer.music.set_volume(0)
-                                elif event.key == pygame.K_z:
-                                    pygame.mixer.music.set_volume(0.5)
